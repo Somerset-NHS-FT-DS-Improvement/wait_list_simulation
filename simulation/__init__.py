e7a86c6253c33d462de2f82c4c35fa9218f3e0d1
Chris Sampson <christopher.sampson@somersetft.nhs.uk>
2024-10-02 06:15:22 +0100

    Updating simulation with rott parameterisation

--- simulation/__init__.py
+++ simulation/__init__.py
@@ -4,6 +4,7 @@ from .patient_management.priority import PriorityCalculator
 from .simulation_components import (Capacity, PatientGenerator,
                                     generate_simulation_graph,
                                     get_appointment_duration)
+from .patient_management.rott import RemovalOtherThanTreatment
 
 
 def parameterise_simulation(
@@ -15,13 +16,15 @@ def parameterise_simulation(
     cancellation_rate,
     emergency_rate,
     fu_rate,
-    rott_rate,
     length_of_simulation,
     dna_rng,
     cancellation_rng,
     emergency_rng,
     fu_rng,
-    rott_rng,
+    rott_sql_query = None,
+    rott_dist_params = None,
+    rott_seed = None,
+    capacity_seed = None,
 ):
     # TODO: Fix this hack... make sfttoolbox available outside of org?
     import sfttoolbox
@@ -29,47 +32,31 @@ def parameterise_simulation(
     pg = PatientGenerator(new_patient_function, start_id=initial_waitlist.shape[0])
 
     pc = PriorityCalculator(priority_order)
+
+    rott = RemovalOtherThanTreatment(length_of_simulation, seed=rott_seed)
+    if rott_sql_query:
+        rott.setup_sql_distribution(sql_engine=rott_sql_query["engine"], query_string=rott_sql_query["query"])
+    elif rott_dist_params:
+        rott.setup_stochastic_distribution(mean=rott_dist_params["mean"], std_dev=rott_dist_params["stddev"])
+    else:
+        raise Exception("Either rott_sql_query or rott_dist_params must be defined.")
+
+    rott_removals = rott.return_number_of_removals()
+
+    # TODO: parameterise emergency generation
+
     # TODO: Pass all the rates and rngs through
     cap = Capacity(
         resource_matching_function,
         pc,
         dna_rate,
         cancellation_rate,
-        emergency_rate,
         fu_rate,
-        rott_rate,
-        dna_rng,
-        cancellation_rng,
-        emergency_rng,
-        fu_rng,
-        rott_rng,
         initial_waitlist,
+        rott_removals,
+        seed = capacity_seed
     )
 
     G = generate_simulation_graph(cap, get_appointment_duration)
 
     return sfttoolbox.DES.Simulation(G, pg, length_of_simulation)
-
-
-if __name__ == "__main__":
-    # parameterise the mri new patient class -- Mon -- DONE
-    # pass the new object to the above function -- Mon -- DONE
-
-    # get the initial wait list together -- Mon -- DONE
-    # pass to the above function -- Mon -- DONE
-
-    # create a dummy resource matching -- Mon -- DONE
-
-    # priority order -- Mon -- DONE
-    # dna_rate, cancellation_rate, emergency_rate, fu_rate all set to 0 for now -- Mon -- DONE
-    # length_of_simulation -- Mon -- DONE
-
-    # run simulation always increasing waitlist -- Mon
-
-    # Look at altering all the rates -- Mon
-
-    # create metrics to track how much increased -- Tues
-
-    # start to interface with webapp -- Tues
-
-    pass
--- simulation/simulation_components.py
+++ simulation/simulation_components.py
@@ -5,26 +5,16 @@ import pandas as pd
 
 class PatientGenerator:
     def __init__(
-        self, generate_new_patients, emergency_random_number_gen=None, start_id=0
+        self, generate_new_patients, start_id=0
     ):
         self.id = start_id
         self.generate_new_patients = generate_new_patients
-
-        self.emergency_random_number_gen = (
-            emergency_random_number_gen
-            if emergency_random_number_gen
-            else np.random.normal
-        )
-
-        # TODO: take in Emergency rate and seeds.
 
     def generate_patients(self, day_num, day):
         # Emergency and new patients dealt with through the interface with generate_new_patients
         patients = self.generate_new_patients(day_num)
 
-        patients.loc[:, "Pathway"] = [[] for _ in range(patients.shape[0])]
-
-        # TODO: fill in appointment_duration if not present, fill nans etc...
+        patients.loc[:, "pathway"] = [[] for _ in range(patients.shape[0])]
 
         patients.loc[:, "id"] = [*range(self.id, self.id + len(patients))]
         self.id += len(patients) - 1
@@ -36,6 +26,10 @@ class PatientGenerator:
     def __yield_patient(self, iterable_object):
         for _, patient in iterable_object:
             yield patient
+
+class EmergencyPatientGenerator:
+    def __init__(self):
+        pass
 
 
 def get_appointment_duration(patient):
@@ -49,15 +43,10 @@ class Capacity:
         prioritisation_calculator,
         dna_rate,
         cancellation_rate,
-        emergency_rate,
         fu_rate,
-        rott_rate,
-        dna_rng,
-        cancellation_rng,
-        emergency_rng,
-        fu_rng,
-        rott_rng,
         initial_wait_list,
+        rott_removals,
+        seed = None,
     ):
         self.wait_list = initial_wait_list
         self.prioritisation_calculator = prioritisation_calculator
@@ -65,18 +54,14 @@ class Capacity:
 
         self.dna_rate = dna_rate
         self.cancellation_rate = cancellation_rate
-        self.emergency_rate = emergency_rate
         self.fu_rate = fu_rate
-        self.rott_rate = rott_rate
 
-        self.dna_rng = dna_rng
-        self.cancellation_rng = cancellation_rng
-        self.emergency_rng = emergency_rng
-        self.fu_rng = fu_rng
-        self.rott_rng = rott_rng
+        self.rott_removals = rott_removals
+
+        self.rng = np.random.default_rng(seed)
 
         self.wait_list["id"] = [*range(0, self.wait_list.shape[0])]
-        self.wait_list["Pathway"] = [[] for _ in range(self.wait_list.shape[0])]
+        self.wait_list["pathway"] = [[] for _ in range(self.wait_list.shape[0])]
 
         self.metrics = {"maximum_wait_time": [], "wait_list_length": []}
 
@@ -93,23 +78,34 @@ class Capacity:
         )
         self.wait_list = self.wait_list.iloc[indices]
 
-        # TODO: deal with fu, emergency, ROTT, DNA, cancellations, then update below
-        patients_DNA_or_cancel = pd.DataFrame()
+        patients_to_move_on_indices = self.match_resource(self.wait_list)
 
-        patients_to_move_on = self.match_resource(self.wait_list)
-        # TODO: add patients to have fu to the wait list with new FU label
+        patients_to_move_on = self.wait_list[self.wait_list.index.isin(patients_to_move_on_indices)]
+        self.wait_list.drop(index=patients_to_move_on_indices, inplace=True)
+
+        # TODO: add patients to have fu to the wait list with new FU label -- Do this in the resource, send back the dataframe of FUs
+
+        # TODO: Cancel and DNA needs to be taken from the patients to move on -- check the process for DNAs and cancellations )do they go back on the list?)
 
         # Or take patients to move on, and randomly select to have FU appts?
         # Use a geometric distribution, 1/1-p = total num of desired appts, solve for p, then random number lower than p means a fu is done
 
+        rott_patients = self.rng.choice(self.wait_list.index, self.rott_removals[day_num])
+        self.wait_list.drop(index=rott_patients, inplace=True)
+
         self.__update_metrics()
         self.wait_list["days waited"] += 1
 
-        return patients_to_move_on
+        iterable_patients = patients_to_move_on.iterrows()
+        return self.__yield_patient(iterable_patients)
 
     def __update_metrics(self):
         self.metrics["maximum_wait_time"].append(self.wait_list["days waited"].max())
         self.metrics["wait_list_length"].append(self.wait_list.shape[0])
+
+    def __yield_patient(self, iterable_object):
+        for _, patient in iterable_object:
+            yield patient
 
 
 def generate_simulation_graph(capacity, resource):
